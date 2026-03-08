6aee1b456681889ff9799698d731eb339cbe1eb6
x <x@1tz.in>
2026-03-08 21:36:43 +0900

    auto commit

--- Alias/Windows/PowerShell/profile.ps1
+++ Alias/Windows/PowerShell/profile.ps1
@@ -181,9 +181,8 @@ function gsacp {
     }
 
     Write-Host "=== 서브모듈 처리 시작 ===" -ForegroundColor Cyan
-
     git submodule foreach --quiet `
-        "git add . && git diff --cached --quiet || git commit -m '$msg' && git push" 2>$null
+        "branch=\$(git symbolic-ref --short HEAD 2>/dev/null); if [ -z \"\$branch\" ]; then git checkout main 2>/dev/null; fi; git add . && git diff --cached --quiet || git commit -m '$msg' && git push" 2>$null
 
     Write-Host "=== 최상위 repo 처리 ===" -ForegroundColor Cyan
 
