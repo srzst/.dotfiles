b5fd9c1bdbd1dcf5acb5d2e032a1a96ca99f8a62
x <x@1tz.in>
2026-04-03 22:27:08 +0900

    Auto sync from Mac (2026-04-03 22:27:08)

--- common/neovim/lua/config/options.lua
+++ common/neovim/lua/config/options.lua
@@ -1,33 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
--- if vim.fn.has("win32") == 1 then
---   vim.o.shell = "powershell"
---   vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
---   vim.o.shellxquote = ""
---   vim.o.shellquote = ""
---   vim.o.shellpipe = "| Out-File -Encoding UTF8 %s"
---   vim.o.shellredir = "| Out-File -Encoding UTF8 %s"
--- end
-
--- vim.opt.clipboard = "unnamedplus"
-
--- local ok, osc52 = pcall(require, "vim.ui.clipboard.osc52")
--- if ok then
---   vim.g.clipboard = {
---     name = "OSC 52",
---     copy = {
---       ["+"] = osc52.copy("+"),
---       ["*"] = osc52.copy("*"),
---     },
---     paste = {
---       ["+"] = osc52.paste("+"),
---       ["*"] = osc52.paste("*"),
---     },
---   }
--- end
-
 local function setup_clipboard()
   local is_wsl = os.getenv("WSL_DISTRO_NAME") or os.getenv("WSL_INTEROP")
   local is_ssh = os.getenv("SSH_CLIENT") or os.getenv("SSH_TTY") or os.getenv("SSH_CONNECTION")
@@ -69,3 +39,6 @@ vim.api.nvim_create_autocmd("VimEnter", {
   callback = setup_clipboard,
 })
 
+
+vim.opt.wrap = true
+vim.opt.linebreak = true
