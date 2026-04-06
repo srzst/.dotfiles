b2bd33b070d0b79021e33b6804600d7ee4bf8bd9
x <x@1tz.in>
2026-04-06 09:17:41 +0900

    neotree.lua 파일주석처리된 부분 삭제

--- common/neovim/lua/plugins/neo-tree.lua
+++ common/neovim/lua/plugins/neo-tree.lua
@@ -3,27 +3,6 @@
 -- vim.g.loaded_netrwPlugin = 1
 
 
--- return {
---   "nvim-neo-tree/neo-tree.nvim",
---   opts = {
---     window = {
---       mappings = {
---         ["<C-t>"] = function(state)
---           local node = state.tree:get_node()
---           local path = node:get_id()
---           if node.type ~= "directory" then
---             path = vim.fn.fnamemodify(path, ":h")
---           end
---           require("toggleterm").toggle({
---             dir = path,
---             direction = "float",
---           })
---         end,
---       },
---     },
---   },
--- }
-
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
